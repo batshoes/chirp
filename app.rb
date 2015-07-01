0a9bb1b0fc1e048ed5ebf89a857069ce9eb74a92
Stephanie <stephanierenee.mathison@gmail.com>
2015-07-01 17:36:17 -0400

    added profile.erb added edit_profile.erb added to app.rb profile info

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/activerecord'
 require './models'
 
 set :database, "sqlite3:chirp.sqlite3"
+enable :sessions
 
 get '/sign_up' do
   erb :sign_up
@@ -11,15 +12,57 @@ end
 post '/sign_up' do  
   confirmation = params[:confirm_password]
   
-
   if confirmation == params[:user][:password]
   @user = User.create(params[:user])
   "Signed Up! Check your Email #{@user.username}"
 
-
-  else
+	else
     "Uh Uh Ahh"
   end
-
-
 end
+
+get '/profile' do
+  @profile = current_user.profile
+  erb :profile
+end
+
+get '/edit_profile' do
+  @profile = current_user.profile
+  erb :edit_profile
+end
+
+post '/edit_profile' do
+  lname = params[:lname]
+  fname = params[:fname]
+  zip_code = params[:zip_code]
+  occupation = [:occupation]
+  #save profile
+  redirect'/profile'
+end
+
+def current_user
+  if session[:user_id]
+    User.find session[:user_id]
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
