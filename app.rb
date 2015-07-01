1010f56cdbf3a71f039097b6c59b570011a93b22
James Middlemiss <middlemissj.usa@gmail.com>
2015-07-01 17:14:00 -0400

    Added sign_in

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
+require 'rack-flash'
 
 set :database, "sqlite3:chirp.sqlite3"
+
+
+
+get '/' do
+
+  erb :home
+end
+
 
 get '/sign_up' do
   erb :sign_up
@@ -17,3 +26,28 @@ post '/sign_up' do
     "Uh Uh Ahh"
   end
 end
+
+get '/sign_in' do
+  erb :sign_in
+end
+
+post '/sign_in' do
+  puts params.inspect
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.where(username: username).first
+  puts @user
+  if @user.password == password
+    session[:user_id] = @user.id
+    puts session[:user_id] 
+    puts "You logged In!"
+    #flash[:notice] = "Welcome #{@user.username}!"
+    redirect '/'
+  else
+    #flash[:notice] = "Uh Uh Ahh"
+    puts "Uh Uh Ahh"
+    redirect '/sign_up'
+  end
+end
+
