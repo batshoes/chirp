5040d0921c03f068332291001c521f2b9e9d578f
Stephanie <stephanierenee.mathison@gmail.com>
2015-07-06 19:19:39 -0400

    WORKING

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-
 $(function() {
  $(".nav li").mouseover(function () {
-=======
-$(".nav li").mouseover(function () {
->>>>>>> javascript mess
+
       $(this).siblings().addClass("fade");
   }).mouseout(function () {
       $(this).siblings().removeClass("fade");
 });
-<<<<<<< HEAD
 });
-=======
->>>>>>> javascript mess
